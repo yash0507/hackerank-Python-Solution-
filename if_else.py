16af6077180099e002dca9a5a407c9798bc71b35
PUNEET KUMAR VERMA <42946336+yash0507@users.noreply.github.com>
2021-05-07 10:14:59 +0530

    Add files via upload

---
+++ if_else.py
@@ -0,0 +1,11 @@
+import math
+import os
+import random
+import re
+import sys
+if __name__ == '__main__':
+    n = int(input().strip())
+    if n%2==0:
+        print('Not Weird')
+    else:
+        print('Weird')
